1d21eb7f4100ab278cede9cf6ac172eb89462e7a
Shiva Prakash Perumal <avi@shivas-mbp.myfiosgateway.com>
2024-01-30 00:00:44 -0500

    email working

--- weather_data.py
+++ weather_data.py
@@ -1,43 +1,33 @@
 import schedule
 import smtplib
 import requests
-from bs4 import BeautifulSoup
+import json
+from datetime import date, datetime
 
+# t_date = date.today()
+# print(t_date)
 
 def umbrellaReminder():
-    city = "harrison"
-    state = "nj"
-
-    url = "https://www.google.com/search?q=" + "weather" + city + state
-    html = requests.get(url).content
-
-    soup = BeautifulSoup(html, 'html.parser')
-    # temperature = soup.find('div',
-    #                         attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
-    # time_sky = soup.find('div',
-    #                      attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
-    temperature_elem = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'})
-    time_sky_elem = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'})
-
-    if temperature_elem and time_sky_elem:
-        temperature = temperature_elem.text
-        time_sky = time_sky_elem.text
-
-
-        sky = time_sky.split('\n')[1]
-
-        if sky == "Rainy" or sky == "Rain And Snow" or sky == "Showers" or sky == "Haze" or sky == "Cloudy":
+    zipcode = '07029'
+    country = 'US'
+    apikey = apikey
+    url = f"https://api.tomorrow.io/v4/weather/realtime?location={zipcode}%20{country}&apikey={apikey}"
+    x = requests.get(url).content
+    if x:
+        x = x.decode('utf8')
+        x = json.loads(x)
+        if x['data']['values']['weatherCode'] < 2100:
+            print('yes')
             smtp_object = smtplib.SMTP('smtp.gmail.com', 587)
 
             # start TLS for security
             smtp_object.starttls()
-
+            app_password = app_password
             # Authentication
-            smtp_object.login("shivavihs98@gmail.com", "avihs98..")
-            subject = "GeeksforGeeks Umbrella Reminder"
+            smtp_object.login("shivavihs98@gmail.com", app_password)
+            subject = "Umbrella Reminder"
             body = f"Take an umbrella before leaving the house.\
-            Weather condition for today is {sky} and temperature is \
-                    {temperature} in {city}."
+            Weather condition for today is rainy in HARRISON."
             msg = f"Subject:{subject}\n\n{body}\n\nRegards,\nGeeksforGeeks".encode(
                 'utf-8')
 
@@ -49,11 +39,13 @@ def umbrellaReminder():
             smtp_object.quit()
             print("Email Sent!")
         else:
-            print(f"No need for an umbrella today. Weather condition for today is {sky} and temperature is {temperature} in {city}.")
+            print(f"No need for an umbrella today.")
     else:
         print("Unable to retrieve weather information.")
     # Every day at 06:00AM time umbrellaReminder() is called.
-schedule.every().day.at("13:41").do(umbrellaReminder)
+# schedule.every().day.at("14:48").do(umbrellaReminder)
+#
+# while True:
+#     schedule.run_pending()
 
-while True:
-    schedule.run_pending()
+umbrellaReminder()
