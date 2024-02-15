3637cfd7c16a8687329ce333ecadfbd7b28ab221
Shiva Prakash Perumal <avi@shivas-mbp.resource.campus.njit.edu>
2024-02-15 13:35:35 -0500

    add .gitignore

--- weather_data.py
+++ weather_data.py
@@ -1,16 +1,19 @@
+import os
 import schedule
 import smtplib
 import requests
 import json
-from datetime import date, datetime
+from dotenv import load_dotenv
 
-# t_date = date.today()
-# print(t_date)
+load_dotenv()
+
+apikey = os.getenv("API_KEY")
+app_password = os.getenv("APP_PASSWORD")
+user_email = os.getenv("SMTP_USERNAME")
 
 def umbrellaReminder():
     zipcode = '07029'
     country = 'US'
-    apikey = apikey
     url = f"https://api.tomorrow.io/v4/weather/realtime?location={zipcode}%20{country}&apikey={apikey}"
     x = requests.get(url).content
     if x:
@@ -19,33 +22,28 @@ def umbrellaReminder():
         if x['data']['values']['weatherCode'] < 2100:
             print('yes')
             smtp_object = smtplib.SMTP('smtp.gmail.com', 587)
-
             # start TLS for security
             smtp_object.starttls()
-            app_password = app_password
+            app_password = 'rnrx gxhf spdn bvgh'
             # Authentication
-            smtp_object.login("shivavihs98@gmail.com", app_password)
+            smtp_object.login(user_email, app_password)
             subject = "Umbrella Reminder"
             body = f"Take an umbrella before leaving the house.\
             Weather condition for today is rainy in HARRISON."
             msg = f"Subject:{subject}\n\n{body}\n\nRegards,\nGeeksforGeeks".encode(
                 'utf-8')
 
-            # sending the mail
             smtp_object.sendmail("shivavihs98@gmail.com",
                                  "shiva.prakash.perumal@gmail.com", msg)
 
-            # terminating the session
             smtp_object.quit()
             print("Email Sent!")
         else:
             print(f"No need for an umbrella today.")
     else:
         print("Unable to retrieve weather information.")
-    # Every day at 06:00AM time umbrellaReminder() is called.
-# schedule.every().day.at("14:48").do(umbrellaReminder)
-#
-# while True:
-#     schedule.run_pending()
+# Every day at 06:00AM time umbrellaReminder() is called.
+schedule.every().day.at("13:33").do(umbrellaReminder)
 
-umbrellaReminder()
+while True:
+    schedule.run_pending()
